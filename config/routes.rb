84e257fcd4ac23e0816772f5275e5456b013b25b
mrjabba <mrjabba@gmail.com>
2010-09-25 12:36:53 -0500

    initial stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SoccerLeague::Application.routes.draw do
+
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
