1bd92242cdd508462ccfec6af67f23f0c1a681ee
mrjabba <mrjabba@gmail.com>
2010-09-25 12:54:34 -0500

    remoced default rails stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SoccerLeague::Application.routes.draw do
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+    
+  root :to => "pages#home"
+    
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +57,9 @@ SoccerLeague::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+
   # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
