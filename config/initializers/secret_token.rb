660130a17479eeeef4e57862e563137c98c6d922
mrjabba <mrjabba@gmail.com>
2010-09-25 12:09:36 -0500

    rails yumminess

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerLeague::Application.config.secret_token = 'caa31a3e5dc6677c32fe034e6ab06db076270ab0007e2d640dbaf8233699bbc5a0039a9fef2da8c3acd5a876f118400c63c971f57339ea6d11bb753043c5402d'
