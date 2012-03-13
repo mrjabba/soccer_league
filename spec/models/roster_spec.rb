1445660ab1516ab86fd6f226f4d5a79046e42565
mrjabba <mrjabba@gmail.com>
2012-03-12 19:36:23 -0500

    test cleanup around roster

--- spec/models/roster_spec.rb
+++ spec/models/roster_spec.rb
@@ -5,7 +5,7 @@ describe Roster do
   before(:each) do
     @teamstat = Factory(:teamstat)
     @player = Factory(:player)
-    @attr = { :teamstat_id => @teamstat, :player_id => @player } 
+    @attr = { :teamstat_id => @teamstat, :player_id => @player, :created_by_id => 1 }
   end
 
   it "should create a new instance given valid attributes" do
@@ -13,14 +13,14 @@ describe Roster do
   end
 
   it "should require a teamstat" do
-    no_teamstat_roster = Roster.new(@attr.merge(:teamstat_id => ""))
-    no_teamstat_roster.should_not be_valid
+    Roster.new(@attr.merge(:teamstat_id => "")).should_not be_valid
   end
-
 
   it "should require a player" do
-    no_player_roster = Roster.new(@attr.merge(:player_id => ""))
-    no_player_roster.should_not be_valid
+    Roster.new(@attr.merge(:player_id => "")).should_not be_valid
   end
 
+  it "should require created by id" do
+    Roster.new(@attr.merge(:created_by_id => nil)).should_not be_valid
+  end
 end
