4f39c074301994f8bc671d5d452484a2f5f4ea96
mrjabba <mrjabba@gmail.com>
2012-03-12 20:07:41 -0500

    clean up test factories a bit

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,7 +14,6 @@ Factory.sequence :lastname do |n|
   "Van Playerson#{n}"
 end
 
-# By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
   user.username               Factory.next(:username)
   user.email                  Factory.next(:email)
@@ -29,7 +28,6 @@ Factory.define :player do |player|
   player.position                  Player::POSITIONS.values.first
   player.birth_date                  "09/22/1981"
   player.nationality                  "USA"
-#  player.created_by { |user|  user.association(:user, :username => Factory.next(:username), :email => Factory.next(:email)    ) }
   player.created_by_id 1
 end
 
@@ -43,8 +41,6 @@ Factory.define :team do |team|
   team.phone                  "512-123-4567"
   team.website                  "http://foo.com"
   team.email                  "test@foo.com"
-  #useful but this user association seems to slow tests down quite a bit.
-  #team.created_by { |user|  user.association(:user, :username => Factory.next(:username), :email => Factory.next(:email)    ) }
   team.created_by_id 1
 end
 
@@ -55,29 +51,12 @@ Factory.define :league do |league|
 end
 
 Factory.define :teamstat do |teamstat|
-#TODO can we turn off the stats for this since they are calculated on init now?
-  teamstat.wins           0
-  teamstat.losses         0
-  teamstat.ties           0
-  teamstat.goals_for      0
-  teamstat.goals_against  0
-  teamstat.games_played   0
   teamstat.created_by_id 1
   teamstat.team { |team|  team.association(:team, :name => Factory.next(:name)) }
   teamstat.league { |league|  league.association(:league) }
 end
 
 Factory.define :playerstat do |playerstat|
-#TODO can we turn off the stats for this since they are calculated on init now?
-  playerstat.goals           0
-  playerstat.assists           0
-  playerstat.shots           0
-  playerstat.jersey_number           0
-  playerstat.fouls           0
-  playerstat.yellow_cards           0
-  playerstat.red_cards           0
-  playerstat.minutes           0
-  playerstat.saves           0
   playerstat.player { |player|  player.association(:player) }
   playerstat.game { |game|  game.association(:game) }
   playerstat.team { |team|  team.association(:team) }
