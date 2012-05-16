0b2774df39e0aad9a123570bd37f87a1570424a6
mrjabba <mrjabba@gmail.com>
2012-05-15 23:53:18 -0500

    start switching to rspec specs from before to let. look ma, no instance vars

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -18,114 +18,101 @@ describe TeamsController do
       get 'new'
       response.should have_selector("title", :content => "New Team")
     end
-    
   end
   
   describe "GET 'edit'" do
-    
-    before(:each) do
-      @team = FactoryGirl.create(:team)
+    let(:team) do
       sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:team)
     end
     
      it "should be successful" do
-      get :edit, :id => @team
+      get :edit, :id => team
       response.should be_success
     end
     
     it "should have the right title" do
-      get :edit, :id => @team
+      get :edit, :id => team
       response.should have_selector("title", :content => "Edit team")
     end
-    
   end   
   
   describe "PUT 'update'" do
-    before(:each) do
-      @team = FactoryGirl.create(:team)
+    let(:team) do
       sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:team)
     end
 
     describe "failure" do
-      before(:each) do
-        @attr = { :name => "", :address1 => ""}
+      let(:attr) do
+        { :name => "", :address1 => ""}
       end
       
       it "should render the 'edit' page" do
-        put :update, :id => @team, :team => @attr
+        put :update, :id => team, :team => attr
         response.should render_template('edit')        
       end
       
       it "should have the right title" do
-        put :update, :id => @team, :team => @attr
+        put :update, :id => team, :team => attr
         response.should have_selector("title", :content => "Edit team")
       end
-      
     end
 
     describe "success" do
-      before(:each) do
-       @attr = { :name => "Austin Aztex", :address1 => "123 Main St.", 
-              :address2 => "Apt A", :city => "Austin", :state => "TX",
-              :zip => "78704", :phone => "512-123-4567", :website => "http://foo.com", 
-              :email => "test@foo.com" }        
+      let(:attr) do
+         { :name => "Austin Aztex", :address1 => "123 Main St.",
+                :address2 => "Apt A", :city => "Austin", :state => "TX",
+                :zip => "78704", :phone => "512-123-4567", :website => "http://foo.com",
+                :email => "test@foo.com" }
       end
       
       it "should change the team's attributes" do
-        put :update, :id => @team, :team => @attr
+        put :update, :id => team, :team => attr
         team = assigns(:team)
-        @team.reload
-        @team.name.should  == team.name
-        @team.address1.should  == team.address1
-        @team.address2.should  == team.address2
-        @team.city.should  == team.city
-        @team.state.should  == team.state
-        @team.zip.should  == team.zip
-        @team.phone.should  == team.phone
-        @team.website.should  == team.website
-        @team.email.should  == team.email
+        team.reload
+        team.name.should  == team.name
+        team.address1.should  == team.address1
+        team.address2.should  == team.address2
+        team.city.should  == team.city
+        team.state.should  == team.state
+        team.zip.should  == team.zip
+        team.phone.should  == team.phone
+        team.website.should  == team.website
+        team.email.should  == team.email
       end
       
       it "should redirect to the team show page" do
-        put :update, :id => @team, :team => @attr
-        response.should redirect_to(team_path(@team))
+        put :update, :id => team, :team => attr
+        response.should redirect_to(team_path(team))
       end
       
       it "should have a flash message" do
-        put :update, :id => @team, :team => @attr
+        put :update, :id => team, :team => attr
         flash[:success].should =~ /updated/
       end
-      
     end
-
-
   end
-  
- 
-  
-  describe "authentication of team edit/update pages" do
 
-    before(:each) do
-      @team = FactoryGirl.create(:team)
+  describe "authentication of team edit/update pages" do
+    let(:team) do
+      FactoryGirl.create(:team)
     end
 
     describe "for non-signed-in users" do
-
       it "should deny access to 'edit'" do
-        get :edit, :id => @team
+        get :edit, :id => team
         response.should redirect_to(new_user_session_path)
       end
 
       it "should deny access to 'update'" do
-        put :update, :id => @team, :team => {}
+        put :update, :id => team, :team => {}
         response.should redirect_to(new_user_session_path)
       end
     end
   end    
-  
 
   describe "GET 'index'" do
-  
     it "should be successful" do
       get :index
       response.should be_success
@@ -139,90 +126,80 @@ describe TeamsController do
   end
 
   describe "GET 'show'" do
-  
-    before(:each) do
-      @user = FactoryGirl.create(:user)
-      @team = FactoryGirl.create(:team)
+    let(:team) do
+      FactoryGirl.create(:team)
     end
     
     it "should be successful" do
-      get :show, :id => @team
+      get :show, :id => team
       response.should be_success
     end
     
     it "should find the right team" do
-      get :show, :id => @team
-      assigns(:team).should == @team
+      get :show, :id => team
+      assigns(:team).should == team
     end
     
     it "should have the right title" do
-      get :show, :id => @team
-      response.should have_selector("title", :content => @team.name)    
+      get :show, :id => team
+      response.should have_selector("title", :content => team.name)
     end
     
     it "should include the team's name" do
-      get :show, :id => @team
-      response.should have_selector("h2", :content => @team.name)
+      get :show, :id => team
+      response.should have_selector("h2", :content => team.name)
     end
-  
   end
-
 
   describe "POST 'create'" do
       describe "failure" do
-
-        before(:each) do
-        sign_in FactoryGirl.create(:user)
-         @attr = { :name => "", :address1 => ""} 
+        let(:attr) do
+         { :name => "", :address1 => ""}
+         sign_in FactoryGirl.create(:user)
         end
 
         it "should not create a team" do
           lambda do
-            post :create, :team => @attr
+            post :create, :team => attr
           end.should_not change(Team, :count)
         end
 
         it "should have the right title" do
-          post :create, :team => @attr
+          post :create, :team => attr
           response.should have_selector("title", :content => "New Team")
         end
 
         it "should render the 'new' page" do
-          post :create, :team => @attr
+          post :create, :team => attr
           response.should render_template('new')
         end  
-        
       end
       
       describe "success" do
-
-        before(:each) do
-        sign_in FactoryGirl.create(:user)
-         @attr = { :name => "name", :address1 => "123 main"} 
+        let(:attr) do
+          sign_in FactoryGirl.create(:user)
+          { :name => "name", :address1 => "123 main"}
         end
 
         it "should create a team" do
           lambda do
-            post :create, :team => @attr
+            post :create, :team => attr
           end.should change(Team, :count).by(1)
         end
 
         it "should redirect to the team show page" do
-          post :create, :team => @attr
+          post :create, :team => attr
           response.should redirect_to(team_path(assigns(:team)))
         end   
         
         it "should have a flash message" do
-          post :create, :team => @attr
+          post :create, :team => attr
           flash[:success].should =~ /Team created successfully/
         end
-
       end
-
   end
   
   describe "DELETE 'destroy'" do
     #should require special admin role
   end
-
 end
