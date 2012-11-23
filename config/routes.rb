868e1919aace50235b788c44ac4ac66f1e36683a
mrjabba <mrjabba@gmail.com>
2012-11-23 13:49:01 -0600

    mount resque via routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SoccerleagueApp::Application.routes.draw do
+  mount Resque::Server.new, :at => "/resque"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
